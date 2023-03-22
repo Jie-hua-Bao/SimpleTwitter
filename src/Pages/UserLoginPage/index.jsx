6565b0a4ac87c688a840515122fe6022073f8a41
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-03-23 04:35:03 +0800

    feat: 登入功能, 設定共用變數

--- src/Pages/UserLoginPage/index.jsx
+++ src/Pages/UserLoginPage/index.jsx
@@ -1,18 +1,47 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { ReactComponent as AcLogo } from "../../assets/icons/AcLogo.svg";
 import styles from "./UserLoginPage.module.scss";
+import { API_URL } from '../../../constants'
 
 // Components
 import Button from "../../Components/Button";
 import AuthInput from "../../Components/AuthInput";
 
 function UserLoginPage() {
+  const navigate = useNavigate()
+  
   //useState狀態，思考這個頁面需要哪些state 輸入的state跟submit的state
   const [account, setAccount] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null)
   // 事件處理
-  const handleClick = () => {};
+  const handleClick = () => {
+		// 點擊登入發送 POST /api/users/login
+		axios
+			.post(API_URL + '/api/users/login', { email: account, password })
+			.then(response => {
+				// console.log(response)
+				//如果成功就能取得token
+				const token = response.data.data.token
+				// 有拿到token就重新導向到首頁 前端頁面其實別暴露我們的id比較好！ 先放這樣之後再改
+				if (token) navigate('/tweet/:id')
+				//如果伺服器回傳錯誤會直接被丟到catch，所以沒有特別檢查 !token
+			})
+			.catch(err => {
+				//此error來自伺服器的回傳
+				setError(new Error('帳號或密碼錯誤'))
+				// console.log(err)
+			})
+	};
+  useEffect(() => {
+    if (error instanceof Error) {
+      // alert後清空錯誤狀態，或是有其他顯示錯誤的方式我不是很確定AC要啥
+      alert(error.message)
+      setError(null)
+    }
+	}, [error])
   return (
     <div className={styles.container}>
       <div>
